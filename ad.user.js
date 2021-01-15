65f770d3044399719180d83328f0fda5155c03da
Magyar Tamás <tamas.donix@gmail.com>
2021-01-15 13:02:40 +0100

    0.2

--- ad.user.js
+++ ad.user.js
@@ -1,23 +1,20 @@
 // ==UserScript==
 // @name         Canvas AntiDetect
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @downloadURL  https://github.com/imdonix/canvas-antidetect/raw/master/antidetect.js
+// @namespace    https://github.com/imdonix/canvas-antidetect
+// @version      0.2
+// @downloadURL  https://github.com/imdonix/canvas-antidetect/raw/master/ad.user.js
 // @description  Prevent canvas to detect blur and focus events.
 // @author       donix
 // @include      https://canvas.elte.hu/*
 // @grant        none
 // ==/UserScript==
 
-
 (function() {
-    const PREFIX = '[AntiDetect] '
 
-    console.log(PREFIX + 'loading...')
-    
     waitAll()
     .then(() =>
     {
+        log('page loaded.')
         try
         {
             if(jQuery)
@@ -27,24 +24,20 @@
                     if(!checkTrackingEvents())
                     {
                         disableTrackingEvents()
-                        console.log(PREFIX + 'events disabled.')
+                        log('events disabled. !')
                     }                        
                 }, 1000)
             }
         }
         catch(ReferenceError)
         {
-            console.log(PREFIX + 'login page.')
+            log('login page. !')
         }
-    })
-    .catch((err) =>
-    {
-        alert(`AntiDetect cant be loaded - ${err}`)
     })
 
     function waitAll()
     {
-        return new Promise((res, rej) =>
+        return new Promise(res =>
         {
             let check = setInterval(() => 
             {
@@ -54,12 +47,6 @@
                   res()
                 }
             }, 100)
-
-            setTimeout(() => 
-            {
-                clearInterval(check)
-                rej('timeout')
-            }, 5000)
         })
     }
 
@@ -73,6 +60,11 @@
     {
         let events = jQuery._data(window, "events")
         return events.blur === undefined && events.focus === undefined
+    }
+
+    function log(msg)
+    {
+        console.log('[AntiDetect] ' + msg)
     }
 
 })();
